adde0dd134b920c8dabefb288d852dc6b6dd056c
ASD-web <1146649768@qq.com>
2019-11-01 10:09:31 +0800

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="button">
+      asdsadd
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  components: {},
+  computed: {},
+  watch: {}
+};
+</script>
+<style lang="less">
+.about {
+  .button {
+    color: blue;
+  }
+}
+</style>
